d80bba90dc76c3d783df5c1f19ea903f4f6717f9
Another-Ashl3y <another.ashl3y@gmail.com>
2024-10-03 12:53:48 +0100

    added the simulation and improvement bot

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,38 @@
 use rand::prelude::*;
+use std::fmt::Display;
 
-struct Log {
+#[derive(Clone, Copy)]
+pub struct Log {
     base: f64,
     cut: f64,
     pool: f64,
+    result: f64,
 }
 
 impl Log {
-    fn new(base: f64, cut: f64, pool: f64) -> Self {
-        Self { base, cut, pool }
+    fn new(base: f64, cut: f64, pool: f64, result: f64) -> Self {
+        //let payout = base + cut * pool;
+        //println!("{} + {}", result - payout, payout);
+        Self {
+            base,
+            cut,
+            pool,
+            result,
+        }
     }
 }
 
+impl Display for Log {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "In: {:.2} - Out: {:.2} + {:.3}% of {:.0}\n \\ => +{}\n",
+            self.base, self.base, self.cut, self.pool, self.result
+        )
+    }
+}
+
+#[derive(Clone)]
 pub struct Player {
     pub money: f64,
     pub rebet: f64,
@@ -36,13 +57,104 @@ impl Player {
     }
     pub fn cash_in(&mut self, cut: f64, money: f64) {
         self.money += self.pot + cut * money;
-        self.history.push(
-            println!(
-            "Bet: {:.3}%\nIn: {:.0} - Out: {:.0} + {:.3}% of {:.0}\n \\ => +{}",
-            
-        
+        let log = Log::new(self.pot, cut, money, self.money);
+        self.history.push(log);
+        // println!("Rebet: {}\n{}", self.rebet, log); // Logging
     }
     pub fn log_loss(&mut self) {
-        self.history.push(Log::new(base, cut, pool));
+        self.history.push(Log::new(-self.pot, 0.0, 0.0, self.money));
+    }
+    pub fn mutate(&mut self, rng: &mut ThreadRng) {
+        self.rebet += rng.gen_range(-0.1..0.1) * 0.1;
+        self.rebet = self.rebet.clamp(0.0, 1.0);
+        self.believer = rng.gen();
+    }
+    pub fn display_logs(&self) {
+        self.history.iter().for_each(|l| println!("{}", l));
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Pool {
+    pub left: f64,
+    pub right: f64,
+}
+
+impl Pool {
+    pub fn new(left: f64, right: f64) -> Self {
+        Self { left, right }
+    }
+    pub fn total(&self) -> f64 {
+        self.left + self.right
+    }
+}
+
+pub struct Manager {
+    pub players: Vec<Player>,
+    pub pool: Pool,
+}
+
+impl Manager {
+    pub fn new(size: usize, rng: &mut ThreadRng) -> Self {
+        Self {
+            players: (0..size).map(|_| Player::new(rng)).collect(),
+            pool: Pool::new(1600000.0, 12000000.0),
+        }
+    }
+    pub fn step(&mut self, rng: &mut ThreadRng) {
+        let ghost: u8 = rng.gen_range(0..24);
+        let believer_win = ghost == 0 || ghost == 1;
+
+        //println!("Believer Win: {}", believer_win);
+
+        for player in self.players.iter_mut() {
+            player.set_pot();
+            if player.believer && believer_win {
+                player.cash_in(player.pot / self.pool.left, self.pool.right);
+            } else if !believer_win && !player.believer {
+                player.cash_in(player.pot / self.pool.right, self.pool.left);
+            } else {
+                player.log_loss();
+            }
+        }
+    }
+    pub fn reset_player_money(&mut self) {
+        self.players
+            .iter_mut()
+            .for_each(|player| player.money = 50.0);
+    }
+    pub fn get_best(&mut self) -> Player {
+        self.sort_players();
+        self.players[0].clone()
+    }
+    pub fn sort_players(&mut self) {
+        self.players
+            .sort_by(|a, b| a.money.partial_cmp(&b.money).unwrap());
+        self.players.reverse();
+    }
+    pub fn improve_step(&mut self, rng: &mut ThreadRng) {
+        self.reset_player_money();
+        (0..100).for_each(|_| self.step(rng));
+        self.sort_players();
+        let mut players_copy = self.players.clone();
+
+        self.players = (0..self.players.len())
+            .map(|_| players_copy[0].clone())
+            .collect();
+
+        self.players
+            .iter_mut()
+            .for_each(|player| player.mutate(rng));
+
+        self.reset_player_money();
+        (0..100).for_each(|_| self.step(rng));
+        players_copy.sort_by(|a, b| a.money.partial_cmp(&b.money).unwrap());
+        players_copy.reverse();
+
+        println!("{} -> {}", players_copy[0].money, self.players[0].money);
+        if self.players[0].money < players_copy[0].money {
+            self.players = players_copy;
+        }
+        self.reset_player_money();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 use ghost_gambling::*;
+use rand::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = thread_rng();
+    let mut manager = Manager::new(20, &mut rng);
+    (0..100).for_each(|_| manager.step(&mut rng));
+    println!("{}", manager.get_best().money);
+    (0..2000).for_each(|_| manager.improve_step(&mut rng));
+    (0..1000).for_each(|_| manager.step(&mut rng));
+    let best_player = manager.get_best();
+    best_player.display_logs();
+    println!(
+        "{}\nRebet: {}\nBeliever: {}",
+        best_player.money, best_player.rebet, best_player.believer
+    );
 }
